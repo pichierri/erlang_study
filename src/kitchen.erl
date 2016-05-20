499ea9dd99366ee5612f1d424b4699650369f562
Francesco Pichierri <fpsava@gmail.com>
2016-05-20 15:26:31 +0200

    Ch. More On Multiprocessing - State Your State

---
+++ src/kitchen.erl
@@ -0,0 +1,43 @@
+%%%-------------------------------------------------------------------
+%%% @author pichierrif
+%%% @copyright (C) 2016, <COMPANY>
+%%% @doc
+%%%
+%%% @end
+%%% Created : 20. mag 2016 12:33
+%%%-------------------------------------------------------------------
+-module(kitchen).
+-author("pichierrif").
+-compile(export_all).
+
+%% Does not work: there is no state
+fridge1() ->
+  receive
+    {From, {store, _Food}} ->
+      From ! {self(), ok},
+      fridge1();
+    {From, {take, _Food}} ->
+      %% uh....
+      From ! {self(), not_found},
+      fridge1();
+    terminate ->
+      ok
+  end.
+
+fridge2(FoodList) ->
+  receive
+    {From, {store, Food}} ->
+      From ! {self(), ok},
+      fridge2([Food | FoodList]);
+    {From, {take, Food}} ->
+      case lists:member(Food, FoodList) of
+        true ->
+          From ! {self(), {ok, Food}},
+          fridge2(lists:delete(Food, FoodList));
+        false ->
+          From ! {self(), not_found},
+          fridge2(FoodList)
+      end;
+    terminate ->
+      ok
+  end.
